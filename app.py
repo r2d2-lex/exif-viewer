79d732e8e7270f74b4b50965324492b68cf0864c
suser <r2d2-lex@yandex.ru>
2021-04-15 16:01:02 +0500

    view exif info

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from exif import Image
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication
+from collections import namedtuple
 import design
 import os
 import sys
@@ -11,10 +13,26 @@ class MyWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.btnOpen.clicked.connect(self.browse_folder)
+        self.btnExit.clicked.connect(self.exit_program)
+        self.btnDelete.clicked.connect(self.delete_exif_data)
         self.listFiles.itemClicked.connect(self.selection_changed)
 
         self.image_folder = ''
         self.support_extensions = '.jpg', '.jpeg'
+        self.list_exif_files = []
+        self.exif_tags = (
+            ('lens_make', 'Lens make'),
+            ('lens_model', 'Lens model'),
+            ('lens_specification', 'Lens specification'),
+            ('software', 'OS version'),
+            ('gps_latitude', 'gps_latitude'),
+            ('gps_latitude_ref', 'gps_latitude_ref'),
+            ('gps_longitude', 'gps_longitude'),
+            ('gps_longitude_ref', 'gps_longitude_ref'),
+            ('datetime_original', 'datetime_original'),
+            ('subsec_time_original', 'subsec_time_original'),
+            ('offset_time', 'offset_time'),
+        )
 
     def browse_folder(self):
         path = '.'
@@ -26,9 +44,11 @@ class MyWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 self.listFiles.addItem(filename)
 
     def selection_changed(self, item):
+        self.listAttrs.clear()
         print("Вы кликнули: {}".format(item.text()))
         image_full_path = '{}/{}'.format(self.image_folder,item.text())
-        self.show_image(image_full_path)
+        if self.chkPreview.isChecked():
+            self.show_image(image_full_path)
         self.show_exif_info(image_full_path)
 
     def show_exif_info(self, image_path):
@@ -36,9 +56,27 @@ class MyWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
             image_exif = Image(image_file)
             if image_exif.has_exif:
                 status = f"contains EXIF (version {image_exif.exif_version}) information."
+                self.show_exif_tags_info(image_exif)
             else:
                 status = "does not contain any EXIF information."
             print(f"Image {status}")
+
+    def show_exif_tags_info(self, image_exif):
+        print(dir(image_exif))
+        EXIF_TAGS = namedtuple('EXIF_TAGS', 'tag description')
+        for _record in self.exif_tags:
+            record = EXIF_TAGS(*_record)
+            tag = image_exif.get(record.tag, 'Unknown')
+            self.listAttrs.addItem(f'{record.description}: {tag}')
+        # self.listAttrs.addItem(f'{image_exif.datetime_original}.{image_exif.subsec_time_original}')
+        # self.listAttrs.addItem(f'Latitude: {image_exif.gps_latitude} {image_exif.gps_latitude_ref}')
+        # self.listAttrs.addItem(f'Longitude: {image_exif.gps_longitude} {image_exif.gps_longitude_ref}')
+
+    def delete_exif_data(self, image_exif):
+        image_exif.delete_all()
+
+    def delete_exif_tag(self, image_exif, tag):
+        image_exif.delete(tag)
 
     def show_image(self, image_path):
         pix = QPixmap(image_path)
@@ -52,6 +90,10 @@ class MyWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
         scene = QtWidgets.QGraphicsScene(self)
         scene.addItem(item)
         self.graphView.setScene(scene)
+
+    @staticmethod
+    def exit_program():
+        QApplication.quit()
 
 
 def main():
